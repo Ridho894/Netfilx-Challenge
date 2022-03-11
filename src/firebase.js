789d2b99e934704822bd540bd54adbcc8e685fec
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-03-12 04:32:49 +0700

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrE_qNEn3NUbhWuTOQh-U1ppxaioxan6g",
+  authDomain: "netflix-challenge-5df45.firebaseapp.com",
+  projectId: "netflix-challenge-5df45",
+  storageBucket: "netflix-challenge-5df45.appspot.com",
+  messagingSenderId: "264845999988",
+  appId: "1:264845999988:web:911475eea2c592fd1db621",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
