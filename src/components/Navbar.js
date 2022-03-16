3319ee68047eb4435073ef15c83da4b7b9f83303
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-03-16 13:36:47 +0700

    lanjut abis ashar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header>
+      <img
+        src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+        alt=".img"
+      />
+      <img
+        src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+        alt=".img"
+      />
+      <h1>Navbar</h1>
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <main className="">
+      {/* Nav */}
+      <Navbar />
+      {/* Banner */}
+      {/* Row */}
+      <h1>Home</h1>
+    </main>
+  );
+}
+
+export default Home;
